f48a6c4f0064af70aeeb214557c0bb89cbade980
BaranovSev <aldebaran94sev@gmail.com>
2023-10-22 23:20:29 +0300

    Initial Commit

---
+++ ApplePie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplePie
+//
+//  Created by Stepan Baranov on 22.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
